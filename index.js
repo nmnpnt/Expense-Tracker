c7254182be1ad7239eaca10df4fe986577cce4cd
nmnpnt <nmnpnt@gmail.com>
2022-01-15 15:21:15 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+// var table = document.getElementById('expnTable');
+// var form = document.getElementById('expform');
+
+// expform.addEventListener('submit', addExpense);
+
+// function addExpense(){
+
+//     var type = document.getElementById('type').value;
+//     var name = document.getElementById('name').value;
+//     var date = document.getElementById('date').value;
+//     var amount = document.getElementById('amount').value;
+
+//     if( type == 'chooseOne' || name.length <=0 || date == 0 || amount <= 0);{
+//         alert('fields Incomplete');
+//         return;
+//         }
+
+//         else{
+        
+//         }
+//     }
+
+    // function onFormSubmit(){
+    //     var formdata = read();
+    //     insertNewRecord(formdata);
+    // }
+
+    // function read(){
+    //     var formdata = {};
+    //     formdata["type"] = document.getElementById("type").value;
+    //     formdata["name"] = document.getElementById("name").value;
+    //     formdata["date"] = document.getElementById("date").value;
+    //     formdata["amount"] = document.getElementById("amount").value;
+
+    //     return formdata;
+    // }
+
+
+    // function tablev(data){
+    //     var table = document.getElementById("lipa").document.getElementById("expnTable")[0];
+    //     var newRow = table.insertRow(table.length);
+    //     cell1 = newRow.insertCell(0);
+    //     cell1.innerHTML = data.type;
+
+    //     cell2 = newRow.insertCell(1);
+    //     cell2.innerHTML = data.name;
+
+    //     cell3 = newRow.insertCell(2);
+    //     cell3.innerHTML = data.date;
+
+    //     cell4 = newRow.insertCell(3);
+    //     cell4.innerHTML = data.amount;
+    // }
